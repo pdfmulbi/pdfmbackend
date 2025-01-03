d0fc9b6866da8bdfd8338305073248c2a446e8f8
Sam9100 <mhaitsamia@gmail.com>
2025-01-03 22:53:50 +0700

    add url

--- config/cors.go
+++ config/cors.go
@@ -9,6 +9,7 @@ var Origins = []string{
 	"https://www.bukupedia.co.id",
 	"https://naskah.bukupedia.co.id",
 	"https://bukupedia.co.id",
+	"https://pdfmulbi.github.io/pdfm.github.io",
 }
 
 // Fungsi untuk memeriksa apakah origin diizinkan
